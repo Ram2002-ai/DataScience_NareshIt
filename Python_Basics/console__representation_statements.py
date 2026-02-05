951001a1a6be798931708754c0985c4b832d319a
USER <airaml2026@gmail.com>
2026-02-05 13:14:20 +0530

    files modifird

---
+++ Python_Basics/console__representation_statements.py
@@ -0,0 +1,202 @@
+# just run the code then click the red icon then right click debug the code and press F8
+# and check the internal process of memory unit
+
+#if condition
+
+# x=4
+# r=x%2
+#
+# if r==0:
+#     print('Even Number')
+
+
+# x=5
+# r=x%2
+# if r==0:
+#     print('Even Number')
+
+
+# x = 5
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#
+# print('odd number')
+
+
+# x = 8
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#
+# print('odd number')
+
+
+# x = 5
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+# if r == 1:
+#     print('odd number')
+
+
+# x = 5
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+# if r != 0:
+#     print('odd number')
+# if r == 1:
+#     print('odd number')
+
+
+# x = 11
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+# else:
+#     print('odd number')
+
+
+
+# x = 10
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+# else:
+#     print('odd number')
+
+
+# x = 3
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#
+#     if x > 5:
+#         print('greater number')
+#
+# else:
+#     print('Odd Number')
+
+#
+# x = 4
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#
+#     if x > 5:
+#         print('greater number')
+#
+# else:
+#     print('Odd Number')
+
+#
+# x = 4
+# r = x % 2
+#
+# if r == 0:
+#     print('Even number')
+#
+#     if x > 5:
+#         print('greater number')
+#
+#     else:
+#         print('smaller number')
+# else:
+#     print('Odd Number')
+
+
+
+# x = 1
+#
+# if x == 1:
+#     print('one')
+# if x == 2:
+#     print('Two')
+# if x == 3:
+#     print('Three')
+# if x == 4:
+#     print('four')
+
+
+# x = 4
+#
+# if x == 1:
+#     print('one')
+# elif x == 2:
+#     print('Two')
+# elif x == 3:
+#     print('Three')
+# elif x == 4:
+#     print('four')
+
+
+# x = 5
+#
+# if x == 1:
+#     print('one')
+#
+# elif x == 2:
+#     print('Two')
+# elif x == 3:
+#     print('Three')
+# elif x == 4:
+#     print('four')
+#
+# else:
+#     print('number not found')
+
+
+
+# print('data science')
+# print('data science')
+# print('data science')
+# print('data science')
+# print('data science')
+
+
+
+# i = 1          # initializing
+#
+# while i<=5:    # condition
+#     print('data science')
+#     i = i + 1  # increment
+
+
+
+# i = 5          # initializing
+#
+# while i>=1:    # condition
+#     print('data science')
+#     i = i - 1  # increment
+
+
+# i = 1
+#
+# while i <= 5:
+#     print('data science')  # when we mention end then new line will not create
+#     j = 1
+#     while j <= 4:
+#         print('technology')
+#         j = j + 1
+#
+#     i = i + 1
+#     print()
+
+
+i = 1
+while i <= 4 :
+    j = 0
+    while  j <= 3 :
+        print(i*j, end=" ")
+        j += 1
+    print()
+    i += 1
