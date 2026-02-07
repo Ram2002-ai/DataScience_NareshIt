9c34dd6a46c13f433d9da016ef484139cb162531
USER <airaml2026@gmail.com>
2026-02-07 15:09:24 +0530

    this is my work

--- UI/Backend/app.py
+++ UI/Backend/app.py
@@ -2,8 +2,6 @@
 # go to cmd and run python app.py (**arg)
 # you have to set path first
 
-import sys
-a=int(sys.argv[1])
-b=int(sys.argv[2])
-c=a+b
-print(c)
+str='bank'
+for i in str:
+    print(i)
