0637f7eaa3e5612ad062e50e9e6e6893d5bcca8e
USER <airaml2026@gmail.com>
2026-02-11 21:52:26 +0530

    this is  my latest work

--- MySql/sql_practice.sql
+++ MySql/sql_practice.sql
@@ -189,3 +189,145 @@ delete from customers where id=6;-- delete the specific record
 delete from customers ;-- delete all table records
 
 
+
+-- SQL JOINS
+/* combine two or more table records*/
+use nit;
+select*from customers;
+create table orders(oid int not null primary key,
+DATE datetime ,customer_id int,Amount int);
+desc orders;
+insert into  orders values(102,'2009-10-08 00:00:00',3,3000);
+insert into orders values(100,'2009-10-08 00:00:00',3,1500),(101,'2009-11-28 00:00:00',3,1500),(103,'2008-05-08 00:00:00',3,1500);
+select*from orders;
+
+update orders set customer_id=2 where oid=101;
+update orders set customer_id=4 where oid=103;
+
+
+-- join
+select id,name,age ,amount from customers,orders where customers.id=orders.customer_id;
+
+
+-- INNER JOIN
+/*INNER JOIN: returns rows when there is a match in both tables.
+like intersection */
+
+show tables;
+select id,name,amount,date from customers inner join orders on customers.id=orders.customer_id;
+
+
+-- Left join
+/*The SQL LEFT JOIN returns all rows from the left table, even if there are no matches in the right table.*/
+
+select*from customers left join orders on customers.id=orders.customer_id;
+select*from orders left join customers on orders.customer_id=customers.id;
+
+
+
+-- Right Join
+/*The SQL RIGHT JOIN returns all rows from the right table, even if there are no matches in the left table.*/
+select*from customers right join orders on customers.id=orders.customer_id;
+select*from orders right join customers on orders.customer_id=customers.id;
+
+
+-- Full Join
+/*The SQL FULL JOIN combines the results of both left and right outer joins.
+MySQL (because MySQL does not support FULL JOIN)
+*/;
+select*from customers full join orders on customers.id=orders.customer_id;
+
+SELECT  ID, NAME, AMOUNT, DATE 
+     FROM CUSTOMERS 
+     LEFT JOIN ORDERS 
+     ON CUSTOMERS.ID = ORDERS.CUSTOMER_ID 
+UNION ALL 
+     SELECT  ID, NAME, AMOUNT, DATE 
+     FROM CUSTOMERS 
+     RIGHT JOIN ORDERS 
+     ON CUSTOMERS.ID = ORDERS.CUSTOMER_ID
+     
+     
+     
+     
+     
+-- SELF JOIN
+/*The SQL SELF JOIN is used to join a table to itself as if the table were two tables, temporarily renaming at least 
+one table in the SQL statement*/;
+
+select a.id,b.name,a.salary from customers a,customers b where a.salary<b.salary;
+
+
+-- CARTESIAN JOIN
+/*The CARTESIAN JOIN or CROSS JOIN returns the cartesian product of the sets of records from the two or more 
+joined tables.*/
+USE NIT;
+select ID,NAME,AMOUNT,DATE FROM CUSTOMERS,ORDERS;-- THE SAME COLUMNS  PRESENTS IN BOTH TABLE
+
+
+
+-- SQL UNION CLAUSE
+/*The SQL UNION clause/operator is used to combine the results of two or more SELECT statements 
+without returning any duplicate rows. */
+
+select ID,NAME,AMOUNT,DATE FROM CUSTOMERS LEFT JOIN ORDERS ON CUSTOMERS.ID=ORDERS.CUSTOMER_ID
+union 
+select ID,NAME,AMOUNT,DATE FROM CUSTOMERS RIGHT JOIN ORDERS ON CUSTOMERS.ID=ORDERS.CUSTOMER_ID;
+
+
+-- UNION ALL CLAUSE
+/*The UNION ALL operator is used to combine the results of two SELECT statements including duplicate rows. */
+
+select ID,NAME,AMOUNT,DATE FROM CUSTOMERS left join ORDERS ON CUSTOMERS.ID=ORDERS.CUSTOMER_ID
+UNION ALL 
+select ID,NAME,AMOUNT,DATE FROM CUSTOMERS RIGHT JOIN ORDERS ON CUSTOMERS.ID=ORDERS.CUSTOMER_ID;
+
+-- INTERSECT CLAUSE
+/*The SQL INTERSECT clause/operator is used to combine two SELECT statements, but returns rows only from the 
+first SELECT statement that are identical to a row in the second SELECT statement. This means INTERSECT 
+returns only common rows returned by the two SELECT statements.
+
+--  MYSQL DOES NOT SUPPORT INTERSECT IT HAS INNER JOIN*/;
+
+select ID,NAME,AMOUNT,DATE FROM CUSTOMERS LEFT JOIN ORDERS ON CUSTOMERS.ID=ORDERS.CUSTOMER_ID
+INTERSECT
+SELECT ID,NAME,AMOUNT,DATE FROM CUSTOMERS RIGHT JOIN ORDERS ON CUSTOMERS.ID=ORDERS.CUSTOMER_ID;
+
+
+
+-- EXCEPT CLAUSE
+/*The SQL EXCEPT clause/operator is used to combine two SELECT statements and returns rows from the first 
+SELECT statement that are not returned by the second SELECT statement. This means EXCEPT returns only 
+rows, which are not available in second SELECT statement. 
+
+-- MYSQL NOT SUPPORT EXCEPT*/
+
+select ID,NAME,AMOUNT,DATE FROM CUSTOMERS LEFT JOIN ORDERS ON CUSTOMERS.ID=ORDERS.CUSTOMER_ID
+EXCEPT
+SELECT ID,NAME,AMOUNT,DATE FROM CUSTOMERS RIGHT JOIN ORDERS ON CUSTOMERS.ID=ORDERS.CUSTOMER_ID;
+
+
+
+-- SQL NULL VALUES
+/*The SQL NULL is the term used to represent a missing value. A NULL value in a table is a value in a field 
+that appears to be blank.
+A field with a NULL value is a field with no value. It is very important to understand that a NULL value is different 
+than a zero value or a field that contains spaces. */
+
+-- IS NOT NULL OPERATOR
+select ID,NAME,AGE,ADDRESS,SALARY FROM CUSTOMERS WHERE SALARY IS NOT NULL;
+insert INTO ORDERS(OID,CUSTOMER_ID) valueS(105,3),(106,4);
+
+-- IS NULL 
+select * FROM ORDERS WHERE AMOUNT IS NULL;
+
+
+
+-- SQL ALIAS SYNTAX
+/*You can rename a table or a column temporarily by giving another name known as alias.*/
+
+-- TABLE ALIAS
+SELECT C.ID,C.NAME,C.AGE,O.AMOUNT FROM CUSTOMERS AS C, ORDERS AS O WHERE C.ID=O.CUSTOMER_ID;
+
+-- COLUMN ALIAS
+select ID AS CUSTOMER_ID,NAME AS CUSTOMER_NAME FROM CUSTOMERS WHERE SALARY IS NOT NULL;
